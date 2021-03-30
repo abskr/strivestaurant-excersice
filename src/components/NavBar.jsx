8b24c3e5d69f75c2509b9bfe636d7709f57943c0
ABskr <bskr26@gmx.de>
2021-03-30 13:12:10 +0200

    3rd task

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,47 @@
-import { Navbar, Nav } from 'react-bootstrap'
+import React, { Component } from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 
-const NavBar = (props) => (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">{props.title} - Strive for food</Navbar.Brand>
+// const NavBar = (props) => (
+// <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+//     <Navbar.Brand href="#home">{props.title} - Strive for food</Navbar.Brand>
+//     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+//     <Navbar.Collapse id="responsive-navbar-nav">
+//         <Nav className="ml-auto">
+//             {
+//                 props.links.map(link => <Nav.Link key={link} href={'#' + link.toLowerCase()}>{link}</Nav.Link>)
+//             }
+//         </Nav>
+//     </Navbar.Collapse>
+// </Navbar>
+// )
+
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    console.log('rendering NavBar.jsx... (3rd task)');
+  }
+
+  render() {
+    return (
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+          {this.props.title} - Strive for food
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="ml-auto">
-                {
-                    props.links.map(link => <Nav.Link key={link} href={'#' + link.toLowerCase()}>{link}</Nav.Link>)
-                }
-            </Nav>
+          <Nav className="ml-auto">
+            {this.props.links.map((link) => (
+              <Nav.Link key={link} href={'#' + link.toLowerCase()}>
+                {link}
+              </Nav.Link>
+            ))}
+          </Nav>
         </Navbar.Collapse>
-    </Navbar>
-)
+      </Navbar>
+    );
+  }
+}
 
-export default NavBar
+export default NavBar;
 
 // functional components are a touch faster than class based ones
