19cf49642dc9225d1957a8f2893205b6255069d1
ABskr <bskr26@gmx.de>
2021-03-30 13:24:21 +0200

    4th task

--- src/components/DishComments.jsx
+++ src/components/DishComments.jsx
@@ -15,16 +15,35 @@ import { ListGroup } from 'react-bootstrap';
 import React, { Component } from 'react';
 
 class DishComments extends Component {
+  componentDidUpdate = () => {
+    setTimeout(this.setState, 1000);
+    //I'm not sure tho...
+  };
+
   render() {
-    return this.props.selectedDish ? (
-      <ListGroup>
-        <h2>Comments for {this.props.selectedDish.name}</h2>
-        {this.props.selectedDish.comments.map((c) => (
-          <ListGroup.Item key={c.id}>{c.comment}</ListGroup.Item>
-        ))}
-      </ListGroup>
-    ) : (
-      <h1>NOTHING YET</h1>
+    return (
+      //     this.props.selectedDish ? (
+      //   <ListGroup>
+      //     <h2>Comments for {this.props.selectedDish.name}</h2>
+      //     {this.props.selectedDish.comments.map((c) => (
+      //       <ListGroup.Item key={c.id}>{c.comment}</ListGroup.Item>
+      //     ))}
+      //   </ListGroup>
+      // ) : (
+      //   <h1>NOTHING YET</h1>
+      // )
+      <>
+        {this.props.selectedDish && (
+          <ListGroup>
+            <h2>Comments for {this.props.selectedDish.name}</h2>
+            {this.props.selectedDish.comments.map((c) => (
+              <ListGroup.Item key={c.id}>{c.comment}</ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+
+        {!this.props.selectedDish && <h1>NOTHING YET</h1>}
+      </>
     );
   }
 }
